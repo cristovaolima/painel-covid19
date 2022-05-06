d18ce80a031b0e9597548e4bd5b26042fc3ae6d3
Cristovão  Lima <cristovao.lima@widup.com.br>
2022-05-06 09:47:57 -0300

    Initial commit

---
+++ src/containers/Main/components/Panel.jsx
@@ -0,0 +1,39 @@
+import React, { memo } from 'react'
+import RefreshIcon from '../../../assets/images/refresh.svg'
+import { Card, Typography, Button, Select, MenuItem } from '../../../components'
+import COUNTRIES from '../../../commons/constants/countries'
+import { CardPanelContentStyled, ItemStyled } from './style'
+
+const navigatorHasShare = navigator.share
+
+function Panel({ updateAt, onChange, data, country, getCoviddata }) {
+  const { cases, recovered, deaths, todayCases, todayDeaths } = data
+
+  const renderCountries = (country, index) => (
+    <MenuItem key={`country-${index}`} value={country.value}>
+      <ItemStyled>
+        <div>{country.label}</div>
+        <img src={country.flag} alt={`País-${country.label}`} />
+      </ItemStyled>
+    </MenuItem>
+  )
+
+  return (
+    <Card>
+      <CardPanelContentStyled>
+        <div>
+          <Typography variant="h5" component="span" color="primary">COVID19</Typography>
+          <Typography variant="h6" component="span">Painel Coronavírus</Typography>
+          <Typography variant="body2" component="span" color="red">Atualizado em: {updateAt}</Typography>
+          <div className="pt-2">
+            <Select onChange={onChange} value={country}>
+              {COUNTRIES.map(renderCountries)}
+            </Select>
+          </div>
+        </div>
+      </CardPanelContentStyled>
+    </Card>
+  )
+}
+
+export default memo(Panel)
